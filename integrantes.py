05ce24764c528f14b759e6e3f503dd63f5a0721d
Arhy07 <arh.acuna@duocuc.cl>
2023-07-03 19:16:49 +0000

    Integrantes script

---
+++ integrantes.py
@@ -0,0 +1,8 @@
+integrantes = [
+	{'nombre': 'Arhy', 'apellido': 'Acuña'},
+	{'nombre': 'Lilibeth', 'apellido': 'Alcazar'}
+]
+
+for integrantes in integrantes:
+	nombre_completo = f"{integrantes['nombre']} {integrantes['apellido']}"
+	print(nombre_completo)
